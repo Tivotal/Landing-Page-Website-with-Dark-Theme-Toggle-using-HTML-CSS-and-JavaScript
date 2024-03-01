df612b4eab0990c12a089b9f6540a766daf32137
Tivotal <121335284+Tivotal@users.noreply.github.com>
2024-03-01 14:57:01 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+/* Created by Tivotal */
+
+let toggleBtn = document.querySelector(".toggle-btn");
+let body = document.querySelector("body");
+
+toggleBtn.addEventListener("click", () => {
+  body.classList.toggle("dark");
+});
